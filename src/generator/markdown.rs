3b1217f01fc95ec9504fd526b293b230501b2e97
koyashiro <develop@koyashi.ro>
2022-01-18 21:44:13 +0900

    refactor: change to 3-level structure

--- src/generator/markdown.rs
+++ src/generator/markdown.rs
@@ -1,52 +1,65 @@
 use std::fmt::Write;
 
-use crate::testspec::{TestCase, TestCategory, TestSpec};
+use crate::testspec::{PrimaryItem, SecondaryItem, TertiaryItem, TestSpec};
 
 pub fn generate_markdown(spec: &TestSpec) -> Result<String, std::fmt::Error> {
-    let TestSpec { title, categories } = &spec;
+    let TestSpec { title, cases } = &spec;
     let mut buf = String::new();
     writeln!(&mut buf, "# {title}")?;
 
-    for TestCategory { title, cases } in categories {
+    for PrimaryItem {
+        title,
+        children: secondaries,
+    } in cases
+    {
         writeln!(&mut buf)?;
         writeln!(&mut buf, "## {title}")?;
 
-        for TestCase {
+        for SecondaryItem {
             title,
-            operations,
-            confirmations,
-            remarks,
-        } in cases
+            children: tertiaries,
+        } in secondaries
         {
             writeln!(&mut buf)?;
             writeln!(&mut buf, "### {title}")?;
 
-            for (i, operation) in operations.iter().enumerate() {
-                if i == 0 {
-                    writeln!(&mut buf)?;
-                    writeln!(&mut buf, "#### Operations")?;
-                    writeln!(&mut buf)?;
-                }
-                let order = i + 1;
-                writeln!(&mut buf, "{order}. {operation}")?;
-            }
+            for TertiaryItem {
+                title,
+                operations,
+                confirmations,
+                remarks,
+            } in tertiaries
+            {
+                writeln!(&mut buf)?;
+                writeln!(&mut buf, "#### {title}")?;
 
-            for (i, confirmation) in confirmations.iter().enumerate() {
-                if i == 0 {
-                    writeln!(&mut buf)?;
-                    writeln!(&mut buf, "#### Confirmation")?;
-                    writeln!(&mut buf)?;
+                for (i, operation) in operations.iter().enumerate() {
+                    if i == 0 {
+                        writeln!(&mut buf)?;
+                        writeln!(&mut buf, "##### Operations")?;
+                        writeln!(&mut buf)?;
+                    }
+                    let order = i + 1;
+                    writeln!(&mut buf, "{order}. {operation}")?;
                 }
-                writeln!(&mut buf, "- [ ] {confirmation}")?;
-            }
 
-            for (i, remark) in remarks.iter().enumerate() {
-                if i == 0 {
-                    writeln!(&mut buf)?;
-                    writeln!(&mut buf, "#### Remarks")?;
-                    writeln!(&mut buf)?;
+                for (i, confirmation) in confirmations.iter().enumerate() {
+                    if i == 0 {
+                        writeln!(&mut buf)?;
+                        writeln!(&mut buf, "##### Confirmation")?;
+                        writeln!(&mut buf)?;
+                    }
+                    writeln!(&mut buf, "- [ ] {confirmation}")?;
                 }
-                writeln!(&mut buf, "- {remark}")?;
+
+                for (i, remark) in remarks.iter().enumerate() {
+                    if i == 0 {
+                        writeln!(&mut buf)?;
+                        writeln!(&mut buf, "##### Remarks")?;
+                        writeln!(&mut buf)?;
+                    }
+                    writeln!(&mut buf, "- {remark}")?;
+                }
             }
         }
     }
--- src/testspec.rs
+++ src/testspec.rs
@@ -6,7 +6,8 @@ use serde::{Deserialize, Serialize};
 pub struct TestSpec {
     pub title: String,
 
-    pub categories: Vec<TestCategory>,
+    #[serde(default)]
+    pub cases: Vec<PrimaryItem>,
 }
 
 impl FromStr for TestSpec {
@@ -17,14 +18,28 @@ impl FromStr for TestSpec {
 }
 
 #[derive(Debug, Deserialize, Eq, PartialEq, Serialize)]
-pub struct TestCategory {
+pub struct PrimaryItem {
     pub title: String,
 
     #[serde(default)]
-    pub cases: Vec<TestCase>,
+    pub children: Vec<SecondaryItem>,
 }
 
-impl FromStr for TestCategory {
+impl FromStr for PrimaryItem {
+    type Err = serde_yaml::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        serde_yaml::from_str(s)
+    }
+}
+
+#[derive(Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct SecondaryItem {
+    pub title: String,
+
+    pub children: Vec<TertiaryItem>,
+}
+
+impl FromStr for SecondaryItem {
     type Err = serde_yaml::Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         serde_yaml::from_str(s)
@@ -38,7 +53,7 @@ pub type Confirmation = String;
 pub type Remark = String;
 
 #[derive(Debug, Deserialize, Eq, PartialEq, Serialize)]
-pub struct TestCase {
+pub struct TertiaryItem {
     pub title: String,
 
     #[serde(default)]
@@ -51,7 +66,7 @@ pub struct TestCase {
     pub remarks: Vec<Remark>,
 }
 
-impl FromStr for TestCase {
+impl FromStr for TertiaryItem {
     type Err = serde_yaml::Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         serde_yaml::from_str(s)
@@ -63,103 +78,319 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_return_ok() {
-        let cases = [
-            (
-                "
+    fn spec_title_only() {
+        let s = "
 title: Spec title
-categories: []
-",
-                TestSpec {
-                    title: "Spec title".to_string(),
-                    categories: vec![],
-                },
-            ),
-            (
-                "
-title: Spec title
-categories:
-  - title: Category 1
-",
-                TestSpec {
-                    title: "Spec title".to_string(),
-                    categories: vec![TestCategory {
-                        title: "Category 1".to_string(),
-                        cases: vec![],
-                    }],
-                },
-            ),
-            (
-                "
-title: Spec title
-categories:
-  - title: Category 1
-    cases:
-      - title: Case 1
-",
-                TestSpec {
-                    title: "Spec title".to_string(),
-                    categories: vec![TestCategory {
-                        title: "Category 1".to_string(),
-                        cases: vec![TestCase {
-                            title: "Case 1".to_string(),
-                            operations: vec![],
-                            confirmations: vec![],
-                            remarks: vec![],
-                        }],
-                    }],
-                },
-            ),
-            (
-                "
-title: Spec title
-categories:
-  - title: Category 1
-    cases:
-      - title: Case 1
-        operations:
-          - Operation 1
-          - Operation 2
-          - Operation 3
-        confirmations:
-          - Confirmation 1
-          - Confirmation 2
-          - Confirmation 3
-        remarks:
-          - Remark 1
-          - Remark 2
-          - Remark 3
-",
-                TestSpec {
-                    title: "Spec title".to_string(),
-                    categories: vec![TestCategory {
-                        title: "Category 1".to_string(),
-                        cases: vec![TestCase {
-                            title: "Case 1".to_string(),
-                            operations: vec![
-                                "Operation 1".to_string(),
-                                "Operation 2".to_string(),
-                                "Operation 3".to_string(),
-                            ],
-                            confirmations: vec![
-                                "Confirmation 1".to_string(),
-                                "Confirmation 2".to_string(),
-                                "Confirmation 3".to_string(),
-                            ],
-                            remarks: vec![
-                                "Remark 1".to_string(),
-                                "Remark 2".to_string(),
-                                "Remark 3".to_string(),
-                            ],
-                        }],
-                    }],
-                },
-            ),
-        ];
+";
+        let expected = TestSpec {
+            title: "Spec title".to_string(),
+            cases: vec![],
+        };
+        let actual: TestSpec = s.parse().unwrap();
+        assert_eq!(expected, actual);
+    }
 
-        for (s, expected) in cases {
-            let actual: TestSpec = s.parse().unwrap();
-            assert_eq!(expected, actual);
-        }
+    #[test]
+    fn single() {
+        let s = "
+title: Spec title
+
+cases:
+  - title: Primary 1
+    children:
+      - title: Secondary 1-1
+        children:
+          - title: Tertiary 1-1-1
+            operations:
+              - Operation 1-1-1-1
+            confirmations:
+              - Confirmation 1-1-1-1
+            remarks:
+              - Remark 1-1-1-1
+";
+
+        let expected = TestSpec {
+            title: "Spec title".to_string(),
+            cases: vec![PrimaryItem {
+                title: "Primary 1".to_string(),
+                children: vec![SecondaryItem {
+                    title: "Secondary 1-1".to_string(),
+                    children: vec![TertiaryItem {
+                        title: "Tertiary 1-1-1".to_string(),
+                        operations: vec!["Operation 1-1-1-1".to_string()],
+                        confirmations: vec!["Confirmation 1-1-1-1".to_string()],
+                        remarks: vec!["Remark 1-1-1-1".to_string()],
+                    }],
+                }],
+            }],
+        };
+        let actual: TestSpec = s.parse().unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn double() {
+        let s = "
+title: Spec title
+
+cases:
+  - title: Primary 1
+    children:
+      - title: Secondary 1-1
+        children:
+          - title: Tertiary 1-1-1
+            operations:
+              - Operation 1-1-1-1
+              - Operation 1-1-1-2
+            confirmations:
+              - Confirmation 1-1-1-1
+              - Confirmation 1-1-1-2
+            remarks:
+              - Remark 1-1-1-1
+              - Remark 1-1-1-2
+
+          - title: Tertiary 1-1-2
+            operations:
+              - Operation 1-1-2-1
+              - Operation 1-1-2-2
+            confirmations:
+              - Confirmation 1-1-2-1
+              - Confirmation 1-1-2-2
+            remarks:
+              - Remark 1-1-2-1
+              - Remark 1-1-2-2
+
+      - title: Secondary 1-2
+        children:
+          - title: Tertiary 1-2-1
+            operations:
+              - Operation 1-2-1-1
+              - Operation 1-2-1-2
+            confirmations:
+              - Confirmation 1-2-1-1
+              - Confirmation 1-2-1-2
+            remarks:
+              - Remark 1-2-1-1
+              - Remark 1-2-1-2
+
+          - title: Tertiary 1-2-2
+            operations:
+              - Operation 1-2-2-1
+              - Operation 1-2-2-2
+            confirmations:
+              - Confirmation 1-2-2-1
+              - Confirmation 1-2-2-2
+            remarks:
+              - Remark 1-2-2-1
+              - Remark 1-2-2-2
+
+  - title: Primary 2
+    children:
+      - title: Secondary 2-1
+        children:
+          - title: Tertiary 2-1-1
+            operations:
+              - Operation 2-1-1-1
+              - Operation 2-1-1-2
+            confirmations:
+              - Confirmation 2-1-1-1
+              - Confirmation 2-1-1-2
+            remarks:
+              - Remark 2-1-1-1
+              - Remark 2-1-1-2
+
+          - title: Tertiary 2-1-2
+            operations:
+              - Operation 2-1-2-1
+              - Operation 2-1-2-2
+            confirmations:
+              - Confirmation 2-1-2-1
+              - Confirmation 2-1-2-2
+            remarks:
+              - Remark 2-1-2-1
+              - Remark 2-1-2-2
+
+      - title: Secondary 2-2
+        children:
+          - title: Tertiary 2-2-1
+            operations:
+              - Operation 2-2-1-1
+              - Operation 2-2-1-2
+            confirmations:
+              - Confirmation 2-2-1-1
+              - Confirmation 2-2-1-2
+            remarks:
+              - Remark 2-2-1-1
+              - Remark 2-2-1-2
+
+          - title: Tertiary 2-2-2
+            operations:
+              - Operation 2-2-2-1
+              - Operation 2-2-2-2
+            confirmations:
+              - Confirmation 2-2-2-1
+              - Confirmation 2-2-2-2
+            remarks:
+              - Remark 2-2-2-1
+              - Remark 2-2-2-2
+";
+
+        let expected = TestSpec {
+            title: "Spec title".to_string(),
+            cases: vec![
+                PrimaryItem {
+                    title: "Primary 1".to_string(),
+                    children: vec![
+                        SecondaryItem {
+                            title: "Secondary 1-1".to_string(),
+                            children: vec![
+                                TertiaryItem {
+                                    title: "Tertiary 1-1-1".to_string(),
+                                    operations: vec![
+                                        "Operation 1-1-1-1".to_string(),
+                                        "Operation 1-1-1-2".to_string(),
+                                    ],
+                                    confirmations: vec![
+                                        "Confirmation 1-1-1-1".to_string(),
+                                        "Confirmation 1-1-1-2".to_string(),
+                                    ],
+                                    remarks: vec![
+                                        "Remark 1-1-1-1".to_string(),
+                                        "Remark 1-1-1-2".to_string(),
+                                    ],
+                                },
+                                TertiaryItem {
+                                    title: "Tertiary 1-1-2".to_string(),
+                                    operations: vec![
+                                        "Operation 1-1-2-1".to_string(),
+                                        "Operation 1-1-2-2".to_string(),
+                                    ],
+                                    confirmations: vec![
+                                        "Confirmation 1-1-2-1".to_string(),
+                                        "Confirmation 1-1-2-2".to_string(),
+                                    ],
+                                    remarks: vec![
+                                        "Remark 1-1-2-1".to_string(),
+                                        "Remark 1-1-2-2".to_string(),
+                                    ],
+                                },
+                            ],
+                        },
+                        SecondaryItem {
+                            title: "Secondary 1-2".to_string(),
+                            children: vec![
+                                TertiaryItem {
+                                    title: "Tertiary 1-2-1".to_string(),
+                                    operations: vec![
+                                        "Operation 1-2-1-1".to_string(),
+                                        "Operation 1-2-1-2".to_string(),
+                                    ],
+                                    confirmations: vec![
+                                        "Confirmation 1-2-1-1".to_string(),
+                                        "Confirmation 1-2-1-2".to_string(),
+                                    ],
+                                    remarks: vec![
+                                        "Remark 1-2-1-1".to_string(),
+                                        "Remark 1-2-1-2".to_string(),
+                                    ],
+                                },
+                                TertiaryItem {
+                                    title: "Tertiary 1-2-2".to_string(),
+                                    operations: vec![
+                                        "Operation 1-2-2-1".to_string(),
+                                        "Operation 1-2-2-2".to_string(),
+                                    ],
+                                    confirmations: vec![
+                                        "Confirmation 1-2-2-1".to_string(),
+                                        "Confirmation 1-2-2-2".to_string(),
+                                    ],
+                                    remarks: vec![
+                                        "Remark 1-2-2-1".to_string(),
+                                        "Remark 1-2-2-2".to_string(),
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+                PrimaryItem {
+                    title: "Primary 2".to_string(),
+                    children: vec![
+                        SecondaryItem {
+                            title: "Secondary 2-1".to_string(),
+                            children: vec![
+                                TertiaryItem {
+                                    title: "Tertiary 2-1-1".to_string(),
+                                    operations: vec![
+                                        "Operation 2-1-1-1".to_string(),
+                                        "Operation 2-1-1-2".to_string(),
+                                    ],
+                                    confirmations: vec![
+                                        "Confirmation 2-1-1-1".to_string(),
+                                        "Confirmation 2-1-1-2".to_string(),
+                                    ],
+                                    remarks: vec![
+                                        "Remark 2-1-1-1".to_string(),
+                                        "Remark 2-1-1-2".to_string(),
+                                    ],
+                                },
+                                TertiaryItem {
+                                    title: "Tertiary 2-1-2".to_string(),
+                                    operations: vec![
+                                        "Operation 2-1-2-1".to_string(),
+                                        "Operation 2-1-2-2".to_string(),
+                                    ],
+                                    confirmations: vec![
+                                        "Confirmation 2-1-2-1".to_string(),
+                                        "Confirmation 2-1-2-2".to_string(),
+                                    ],
+                                    remarks: vec![
+                                        "Remark 2-1-2-1".to_string(),
+                                        "Remark 2-1-2-2".to_string(),
+                                    ],
+                                },
+                            ],
+                        },
+                        SecondaryItem {
+                            title: "Secondary 2-2".to_string(),
+                            children: vec![
+                                TertiaryItem {
+                                    title: "Tertiary 2-2-1".to_string(),
+                                    operations: vec![
+                                        "Operation 2-2-1-1".to_string(),
+                                        "Operation 2-2-1-2".to_string(),
+                                    ],
+                                    confirmations: vec![
+                                        "Confirmation 2-2-1-1".to_string(),
+                                        "Confirmation 2-2-1-2".to_string(),
+                                    ],
+                                    remarks: vec![
+                                        "Remark 2-2-1-1".to_string(),
+                                        "Remark 2-2-1-2".to_string(),
+                                    ],
+                                },
+                                TertiaryItem {
+                                    title: "Tertiary 2-2-2".to_string(),
+                                    operations: vec![
+                                        "Operation 2-2-2-1".to_string(),
+                                        "Operation 2-2-2-2".to_string(),
+                                    ],
+                                    confirmations: vec![
+                                        "Confirmation 2-2-2-1".to_string(),
+                                        "Confirmation 2-2-2-2".to_string(),
+                                    ],
+                                    remarks: vec![
+                                        "Remark 2-2-2-1".to_string(),
+                                        "Remark 2-2-2-2".to_string(),
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        };
+        let actual: TestSpec = s.parse().unwrap();
+        assert_eq!(expected, actual);
     }
 }
